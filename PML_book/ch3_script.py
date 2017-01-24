67717844c2e69700c2980ae59891c43786795b7e
Bakeforfun <bakeforfun@gmail.com>
2017-01-24 18:29:15 +0300

    Chapter 3 finished

--- PML_book/ch3_script.py
+++ PML_book/ch3_script.py
@@ -5,15 +5,23 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 from PML.ch3_helpers import plot_decision_regions
 
+# Load data
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
+# Standardize data
 sc = StandardScaler()
 sc.fit(X_train)
 X_train_std = sc.transform(X_train)
@@ -36,6 +44,10 @@ plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.show()
+plt.close()
+
+
+
 
 # Training a logistic regression via scikit-learn
 def sigmoid(z):
@@ -51,6 +63,7 @@ plt.ylim(-0.1, 1.1)
 plt.xlabel('z')
 plt.ylabel('$\phi (z)$')
 plt.show()
+plt.close()
 
 lr = LogisticRegression(C=1000.0, random_state=0)
 lr.fit(X_train_std, y_train)
@@ -61,8 +74,10 @@ plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.show()
+
 lr.predict_proba(X_test_std[0,:])
 
+# Plot regularization against parameters
 weights, params = [], []
 for c in np.arange(-5, 5):
     lr = LogisticRegression(C=10**c, random_state=0)
@@ -79,3 +94,137 @@ plt.xlabel('C')
 plt.legend(loc='upper left')
 plt.xscale('log')
 plt.show()
+plt.close()
+
+
+
+
+# Training support vector machines via scikit-learn
+svm = SVC(kernel='linear', C=1.0, random_state=0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std,
+                      y_combined, classifier=svm,
+                      test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+plt.close()
+
+# Arternative implementation in scikit-learn using stochastic gradient descent
+ppn = SGDClassifier(loss='perceptron')
+lr = SGDClassifier(loss='log')
+svm = SGDClassifier(loss='hinge')
+
+# Training kernel support vector machines via scikit-learn
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+plt.scatter(X_xor[y_xor==1, 0], X_xor[y_xor==1, 1],
+            c='b', marker='x', label='1')
+plt.scatter(X_xor[y_xor==-1, 0], X_xor[y_xor==-1, 1],
+            c='r', marker='s', label='-1')
+plt.ylim(-3.0)
+plt.legend()
+plt.show()
+plt.close()
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
+plt.close()
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.2, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+plt.close()
+
+svm = SVC(kernel='rbf', random_state=0, gamma=100.0, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+plt.close()
+
+
+
+# Decision tree learning
+
+
+def gini(p):
+    return (p)*(1 - (p)) + (1 - p)*(1 - (1-p))
+
+
+def entropy(p):
+    return - p*np.log2(p) - (1 - p)*np.log2((1 - p))
+
+
+def error(p):
+    return 1 - np.max([p, 1 - p])
+
+
+x = np.arange(0.0, 1.0, 0.01)
+ent = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e*0.5 if e else None for e in ent]
+err = [error(i) for i in x]
+fig = plt.figure()
+ax = plt.subplot(111)
+for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err],
+                          ['Entropy', 'Entropy (scaled)', 'Gini Impurity', 'Misclassification Error'],
+                          ['-', '-', '--', '-.'],
+                          ['black', 'lightgray','red', 'green', 'cyan']):
+    line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=False)
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('Impurity Index')
+plt.show()
+plt.close()
+
+# Training a decision tree via scikit-learn
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier=tree, test_idx=range(105,150))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
+plt.close()
+
+export_graphviz(tree, out_file='tree.dot', feature_names=['petal length', 'petal width'])
+
+
+
+# Training a random forest via scikit-learn
+forest = RandomForestClassifier(criterion='entropy', n_estimators=10, random_state=1, n_jobs=2)
+forest.fit(X_train, y_train)
+plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105,150))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
+plt.close()
+
+# Training a K-nearest neighbors classifier via scikit-learn
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+knn.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=knn, test_idx=range(105,150))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
+plt.close()
